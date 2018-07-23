24885e72714b7b0ec4dc29daff471d92f1658b19
Viki Zhang <onlyzj@gmail.com>
2018-07-23 17:03:59 +0800

    the map method error still exsit, I can not figure out why

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -4,23 +4,27 @@ import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import PlayList from "../PlayList/Playlist";
 class App extends Component {
-  state = {
-    searchResults: [
-      { name: "song A", artist: "artist A", album: "Album A", id: 1 },
-      { name: "song B", artist: "artist B", album: "Album B", id: 2 },
-      { name: "song C", artist: "artist C", album: "Album C", id: 3 },
-      { name: "song D", artist: "artist D", album: "Album D", id: 4 },
-      { name: "song E", artist: "artist E", album: "Album E", id: 5 }
-    ],
-    playlistName: "stronger",
-    playlistTracks: [
-      { name: "song A1", artist: "artist A1", album: "Album A1", id: 11 },
-      { name: "song B1", artist: "artist B1", album: "Album B1", id: 21 },
-      { name: "song C1", artist: "artist C1", album: "Album C1", id: 32 },
-      { name: "song D1", artist: "artist D1", album: "Album D1", id: 42 },
-      { name: "song E1", artist: "artist E1", album: "Album E1", id: 52 }
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        { name: "song A", artist: "artist A", album: "Album A", id: 1 },
+        { name: "song B", artist: "artist B", album: "Album B", id: 2 },
+        { name: "song C", artist: "artist C", album: "Album C", id: 3 },
+        { name: "song D", artist: "artist D", album: "Album D", id: 4 },
+        { name: "song E", artist: "artist E", album: "Album E", id: 5 }
+      ],
+      playlistName: "stronger",
+      playlistTracks: [
+        { name: "song A1", artist: "artist A1", album: "Album A1", id: 11 },
+        { name: "song B1", artist: "artist B1", album: "Album B1", id: 21 },
+        { name: "song C1", artist: "artist C1", album: "Album C1", id: 32 },
+        { name: "song D1", artist: "artist D1", album: "Album D1", id: 42 },
+        { name: "song E1", artist: "artist E1", album: "Album E1", id: 52 }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
--- src/Components/TrackList/TrackList.jsx
+++ src/Components/TrackList/TrackList.jsx
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import Track from "../Track/Track";
 import "./TrackList.css";
 class TrackList extends Component {
-  state = {};
   render() {
     return (
       <div className="TrackList">
-        {/* You will add a map method that renders a set of Track components  */}
+        {this.props.tracks.map(track => {
+          // Todo:TypeError: Cannot read property 'map' of undefined
+          return <Track key={track.id} track={track} />;
+        })}
       </div>
     );
   }
