eb07356f2f359d256b59ff31e7a270208a93c224
Viki Zhang <onlyzj@gmail.com>
2018-07-23 16:36:54 +0800

    come to step 33, the most diffcult part. the map method

--- src/Components/SearchResults/SearchResults.jsx
+++ src/Components/SearchResults/SearchResults.jsx
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 import "./SearchResults.css";
 import TrackList from "../TrackList/TrackList";
 class SearchResults extends Component {
-  state = {};
   render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        <TrackList />
+        <TrackList SearchResults={this.props.searchResults} />
       </div>
     );
   }
