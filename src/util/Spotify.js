c474e3fca76ecfb08ea2a85f30178fecf8c4fac9
Viki Zhang <onlyzj@gmail.com>
2018-08-01 21:39:25 +0800

    write the spotify.js. have no idea what to do.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,10 +1,34 @@
-class Spotify {
-  constructor(accessToken) {
-    this.accessToken = accessToken;
-  }
+const cilentID = 6ac2ce2fcfdb4a9db569680b9dd9bde2;
+const RedirectURI = "http://localhost:3000/"
+
+
+const Spotify = {
   getAccessToken() {
-    if (this.accessToken.length > 0) return this.accessToken;
+    if (accessToken) {
+      return accessToken;
+    }
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);}}
+
+
+
+    }
+
+const search = async () => {
+  try {
+    const response = await fetch(https://api.spotify.com/v1/search?type=track&q=TERM);
+    if (response.ok) {
+    // do something
+    }
+  }
+  catch (error) {
+    console.log(error);
   }
 }
+
 
 export default Spotify;
