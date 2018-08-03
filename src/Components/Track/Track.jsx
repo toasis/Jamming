348bd030b56bfc550fe4611c178aa270f67b93a7
Viki Zhang <onlyzj@gmail.com>
2018-08-03 14:22:07 +0800

    Now the search method in app works

--- src/Components/Track/Track.jsx
+++ src/Components/Track/Track.jsx
@@ -25,13 +25,12 @@ class Track extends Component {
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3>{this.props.track.id} </h3>
+          <h3>{this.props.track.name} </h3>
           <p>
             {this.props.track.artist} | {this.props.track.album}
           </p>
         </div>
         {this.renderAction()}
-        {/* {this.addTrack} */}
       </div>
     );
   }
