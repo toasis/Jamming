7c750c95501efa023698932ccc74197bd6a747f9
Viki Zhang <onlyzj@gmail.com>
2018-08-14 18:29:57 +0800

    Refact Track.jsx

--- src/Components/Track/Track.jsx
+++ src/Components/Track/Track.jsx
@@ -1,27 +1,38 @@
 import React, { Component } from "react";
 import "./Track.css";
 class Track extends Component {
-  renderAction = () => {
-    if (this.props.isRemoval)
-      return (
-        <a className="Track-action" onClick={this.removeTrack}>
-          -
-        </a>
-      );
-    return (
-      <a className="Track-action" onClick={this.addTrack}>
-        +
-      </a>
-    );
-  };
-  addTrack = () => {
-    return this.props.onAdd(this.props.track);
-  };
-  removeTrack = () => {
-    return this.props.onRemove(this.props.track);
-  };
+  // renderAction = () => {
+  //   if (this.props.isRemoval)
+  //     return (
+  //       <a className="Track-action" onClick={this.props.removeTrack}>
+  //         -
+  //       </a>
+  //     );
+  //   return (
+  //     <a className="Track-action" onClick={this.addTrack}>
+  //       +
+  //     </a>
+  //   );
+  // };
+  // addTrack = () => {
+  //   return this.props.onAdd(this.props.track);
+  // };
+  // removeTrack = () => {
+  //   return this.props.onRemove(this.props.track);
+  // };
 
   render() {
+    const buttonAdd = (
+      <a className="Track-action" onClick={this.props.onAdd}>
+        Remove -
+      </a>
+    );
+    const buttonRemove = (
+      <a className="Track-action" onClick={this.props.onRemove}>
+        Add +
+      </a>
+    );
+    console.log(this.props);
     return (
       <div className="Track">
         <div className="Track-information">
@@ -30,7 +41,8 @@ class Track extends Component {
             {this.props.track.artist} | {this.props.track.album}
           </p>
         </div>
-        {this.renderAction()}
+        {/* {this.renderAction()} */}
+        {this.props.isRemoval ? buttonAdd : buttonRemove}
       </div>
     );
   }
