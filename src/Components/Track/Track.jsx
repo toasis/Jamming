479660d9be371e50168c6be6d63b0124e2b70b62
Viki Zhang <onlyzj@gmail.com>
2018-08-17 16:58:38 +0800

    transfer Track component to stateless conponment.

--- src/Components/Track/Track.jsx
+++ src/Components/Track/Track.jsx
@@ -1,32 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Track.css";
-class Track extends Component {
-  render() {
-    const { onAdd, onRemove, track, isRemoval } = this.props;
 
-    const buttonAdd = (
-      <a className="Track-action" onClick={onAdd}>
-        +
-      </a>
-    );
-    const buttonRemove = (
-      <a className="Track-action" onClick={onRemove}>
-        -
-      </a>
-    );
+const Track = props => {
+  const { onAdd, onRemove, track, isRemoval } = props;
 
-    return (
-      <div className="Track">
-        <div className="Track-information">
-          <h3>{track.name} </h3>
-          <p>
-            {track.artist} | {track.album}
-          </p>
-        </div>
-        {isRemoval ? buttonRemove : buttonAdd}
+  const buttonAdd = (
+    <a className="Track-action" onClick={onAdd}>
+      +
+    </a>
+  );
+  const buttonRemove = (
+    <a className="Track-action" onClick={onRemove}>
+      -
+    </a>
+  );
+  return (
+    <div className="Track">
+      <div className="Track-information">
+        <h3>{track.name} </h3>
+        <p>
+          {track.artist} | {track.album}
+        </p>
       </div>
-    );
-  }
-}
+      {isRemoval ? buttonRemove : buttonAdd}
+    </div>
+  );
+};
 
 export default Track;
