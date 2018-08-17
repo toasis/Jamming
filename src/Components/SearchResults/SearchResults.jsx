4a3de8fe7a4d105d8e8e81e84278977e69e68240
Viki Zhang <onlyzj@gmail.com>
2018-08-17 16:48:07 +0800

    transfer SearchResults conponment to stateless conponent

--- src/Components/SearchResults/SearchResults.jsx
+++ src/Components/SearchResults/SearchResults.jsx
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import "./SearchResults.css";
 import TrackList from "../TrackList/TrackList";
 
-class SearchResults extends Component {
-  render() {
-    const { searchResults, onAdd, isRemoval } = this.props;
+const SearchResults = props => {
+  const { searchResults, onAdd, isRemoval } = props;
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList tracks={searchResults} onAdd={onAdd} isRemoval={isRemoval} />
+    </div>
+  );
+};
 
-    return (
-      <div className="SearchResults">
-        <h2>Results</h2>
-        <TrackList tracks={searchResults} onAdd={onAdd} isRemoval={isRemoval} />
-      </div>
-    );
-  }
-}
 export default SearchResults;
