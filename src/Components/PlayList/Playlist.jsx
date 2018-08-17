38b97952daf7c37b2794e97d21977676a900c50c
Viki Zhang <onlyzj@gmail.com>
2018-08-17 16:44:03 +0800

    transfer Playlist.jsx to stateless component

--- src/Components/PlayList/Playlist.jsx
+++ src/Components/PlayList/Playlist.jsx
@@ -1,34 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import TrackList from "../TrackList/TrackList";
 import "./Playlist.css";
-class PlayList extends Component {
 
-  render() {
-    const { playlistName, playlistTracks, onRemove, onSubmit, onChange } = this.props;
-
-    return (
-
-      <div className="Playlist">
-        <label>
-          Update playlist name below
-          <input
-            value={playlistName}
-            id="playlistName"
-            type="text"
-            onChange={e=> onChange(e)}
-          />
-        </label>
-        <TrackList
-          tracks={playlistTracks}
-          onRemove={onRemove}
-          isRemoval="true"
+const Playlist = props => {
+  const { playlistName, playlistTracks, onRemove, onSubmit, onChange } = props;
+  return (
+    <div className="Playlist">
+      <label>
+        Update playlist name below
+        <input
+          value={playlistName}
+          id="playlistName"
+          type="text"
+          onChange={e => onChange(e)}
         />
-        <a className="Playlist-save" onSubmit={(e)=>onSubmit(e)}>
-          SAVE TO SPOTIFY
-        </a>
-      </div>
-    );
-  }
-}
+      </label>
+      <TrackList tracks={playlistTracks} onRemove={onRemove} isRemoval="true" />
+      <a className="Playlist-save" onSubmit={e => onSubmit(e)}>
+        SAVE TO SPOTIFY
+      </a>
+    </div>
+  );
+};
 
-export default PlayList;
+export default Playlist;
